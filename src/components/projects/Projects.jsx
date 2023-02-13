c546b5ca7306c07e83c904e453747723743e4a2f
John Romero <romerojohn1012@gmail.com>
2023-02-13 22:57:45 +0100

    added walkin image on the projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,16 @@
+import walkin from '../../assets/Mockup Walkin (1).png'
+import './projects.css'
+
 const projects = () => {
   return (
     <div className="project-container">
       <div className="title">
         <h2>Projects</h2>
+      </div>
+      <div className="projects">
+        <div className="final-project">
+          <img src= {walkin} alt="" />
+        </div>
       </div>
     </div>
    );
