015962ff30a2697a2d9bad400372b4a3c97c6ea5
John Romero <romerojohn1012@gmail.com>
2023-02-14 00:23:14 +0100

    added a button to both cards of the projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,12 +13,18 @@ const projects = () => {
           <div className="final-project">
             <img src= {walkin} alt="" />
             <p>Walkin is a mobile app that helps people to find a table for tonight to make a reservation.</p>
+            <div className="button">
+              <button>Learn More</button>
+            </div>
           </div>
         </div>
         <div className="card">
           <div className="airbnb-clone">
             <img src={clone} alt="" />
             <p>AirBroomNBroom (AirBnB clone) is a market place for broomstick rental.</p>
+            <div className="button">
+              <button>Learn More</button>
+            </div>
           </div>
         </div>
       </div>
