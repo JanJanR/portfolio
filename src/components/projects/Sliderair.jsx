552102054fe105f826aa0d3e6a27a32e88ed2f16
John Romero <romerojohn1012@gmail.com>
2023-02-17 23:10:05 +0100

    better style for the carousel

--- src/components/projects/Sliderair.jsx
+++ src/components/projects/Sliderair.jsx
@@ -4,7 +4,7 @@ import air1 from '../../assets/airbroomnbroom1.png'
 import air2 from '../../assets/airbroomnbroom2.png'
 import air3 from '../../assets/airbroomnbroom3.png'
 import air4 from '../../assets/airbroomnbroom4.png'
-
+import './sliderair.css'
 
 const Sliderair = () => {
   return (
@@ -12,16 +12,16 @@ const Sliderair = () => {
       <Carousel showThumbs={false} useKeyboardArrows={true}
       showStatus={false} showArrows={true} infiniteLoop={true}>
         <div className="eachSlideair">
-          <img src={air1} alt="air1" className="imageSlider" />
+          <img src={air1} alt="air1" />
         </div>
         <div className="eachSlideair">
-          <img src={air2} alt="air2" className="imageSlider" />
+          <img src={air2} alt="air2" />
         </div>
         <div className="eachSlideair">
-          <img src={air3} alt="air3" className="imageSlider" />
+          <img src={air3} alt="air3" />
         </div>
         <div className="eachSlideair">
-          <img src={air4} alt="air4" className="imageSlider" />
+          <img src={air4} alt="air4" />
         </div>
       </Carousel>
     </div>
