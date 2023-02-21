aa55e79a480c321485a1ef02189601ca9bc87224
John Romero <romerojohn1012@gmail.com>
2023-02-21 15:41:16 +0100

    added typewritting effect

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,12 +1,22 @@
 import './landing.css'
 import memoji from '../../assets/memoji hi.png'
+import Typewriter from 'typewriter-effect';
+
 
 const landing = () => {
   return (
       <div className='intro' >
         <h1>
           Hi There! I'm John Romero,
-          I'm a Full Stack Developer
+          <span>
+          <Typewriter
+            options={{
+              strings: ["I'm a Full Stack Developer"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+          </span>
         </h1>
         <img src= { memoji } alt="" />
       </div>
