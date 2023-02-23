81eb3e8f702542fb2e1f56b1766ca398f3df5260
John Romero <romerojohn1012@gmail.com>
2023-02-23 02:55:39 +0100

    added some style in the background

--- src/components/particles/Background.jsx
+++ src/components/particles/Background.jsx
@@ -18,122 +18,122 @@ const ParticlesBackground = () => {
       init={particlesInit}
       loaded={particlesLoaded}
       options={{
-        "fullScreen": {
-            "enable": true,
-            "zIndex": -1
+        fullScreen: {
+            enable: true,
+            zIndex: -1
         },
-        "particles": {
-            "number": {
-                "value": 10,
-                "density": {
-                    "enable": false,
-                    "value_area": 800
+        particles: {
+            number: {
+                value: 10,
+                density: {
+                    enable: false,
+                    value_area: 800
                 }
             },
-            "color": {
-                "value": "#fff"
+            color: {
+                value: "#E4C88E"
             },
-            "shape": {
-                "type": "star",
-                "options": {
-                    "sides": 5
+            shape: {
+                type: "star",
+                options: {
+                    sides: 5
                 }
             },
-            "opacity": {
-                "value": 0.8,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
+            opacity: {
+                value: 0.8,
+                random: false,
+                anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
                 }
             },
-            "size": {
-                "value": 4,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
+            size: {
+                value: 4,
+                random: false,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false
                 }
             },
-            "rotate": {
-                "value": 0,
-                "random": true,
-                "direction": "clockwise",
-                "animation": {
-                    "enable": true,
-                    "speed": 5,
-                    "sync": false
+            rotate: {
+                value: 0,
+                random: true,
+                direction: "clockwise",
+                animation: {
+                    enable: true,
+                    speed: 5,
+                    sync: false
                 }
             },
             "line_linked": {
-                "enable": true,
-                "distance": 600,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 2
+                enable: true,
+                distance: 600,
+                color: "#E4C88E",
+                opacity: 0.4,
+                width: 2
             },
-            "move": {
-                "enable": true,
-                "speed": 2,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
+            move: {
+                enable: true,
+                speed: 2,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
                 }
             }
         },
-        "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": ["grab"]
+        interactivity: {
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: ["grab"]
                 },
-                "onclick": {
-                    "enable": false,
-                    "mode": "bubble"
+                onclick: {
+                    enable: false,
+                    mode: "bubble"
                 },
-                "resize": true
+                resize: true
             },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
+            modes: {
+                grab: {
+                    distance: 400,
+                    line_linked: {
+                        opacity: 1
                     }
                 },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
+                bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3
                 },
-                "repulse": {
-                    "distance": 200
+                repulse: {
+                    distance: 200
                 },
-                "push": {
-                    "particles_nb": 4
+                push: {
+                    particles_nb: 4
                 },
-                "remove": {
-                    "particles_nb": 2
+                remove: {
+                    particles_nb: 2
                 }
             }
         },
-        "retina_detect": true,
-        "background": {
-            "color": "#111",
-            "image": "",
-            "position": "50% 50%",
-            "repeat": "no-repeat",
-            "size": "cover"
+        retina_detect: true,
+        background: {
+            color: "#111",
+            image: "",
+            position: "50% 50%",
+            repeat: "no-repeat",
+            size: "cover"
         }
     }}
     />
